bc5558f42509bb80907b8bfeb26a4844b350739b
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-04-03 20:14:31 +0200

    Set up header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,42 @@
-import { Container, Navbar, Stack } from "react-bootstrap";
+import { Button, Container, Col, Navbar, Nav, Row, Stack } from "react-bootstrap";
 
 export default function Header() {
+    const links = [
+        {
+            "name": "Twitter",
+            "url": "https://www.twitter.com"
+        },
+        {
+            "name": "YouTube",
+            "url": "https://www.twitter.com"
+        },
+        {
+            "name": "Instagram",
+            "url": "https://www.twitter.com"
+        },
+    ]
+
     return (
         <Navbar sticky="top">
-            <Stack direction="horizontal">
-                <Navbar.Brand><h1>NyanDoggo</h1></Navbar.Brand>
-                <p>I make the internet worse</p>
-            </Stack>
+            <Row className="w-100">
+                <Col>
+                    <Nav>
+                        <Navbar.Brand><h1>NyanDoggo.com</h1></Navbar.Brand>
+                        <p>Making the internet worse</p>
+                    </Nav>
+                </Col>
+                <Col >
+                    <Nav className="justify-content-end">
+                        <Stack direction="horizontal" className="sm-auto">
+                            {links.map(link => {
+                                return (
+                                    <Button size="lg" href={link.url} target="_blank">{link.name}</Button>
+                                );
+                            })}
+                        </Stack>
+                    </Nav>
+                </Col>
+            </Row>
         </Navbar>
     );
 }
