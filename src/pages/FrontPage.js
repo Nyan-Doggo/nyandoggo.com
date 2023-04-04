52dfe33d0fccc81ca4c670c0d6cfe204fb483b92
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-04-04 02:06:43 +0200

    added crypto stuff, made it pwetty

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -2,18 +2,32 @@ import { Col, Container, Row } from "react-bootstrap";
 import Header from "../components/header/Header";
 import AboutMeTable from "../components/body/AboutMeTable";
 import AboutMeBio from "../components/body/AboutMeBio";
+import CryptoAbout from "../components/body/CryptoAbout";
 
 export default function FrontPage() {
     return (
         <Container>
             <Header />
             <hr />
-            <Row>
+            <Row >
                 <Col>
                     <AboutMeTable />
                 </Col>
+                <Col className="d-flex align-items-center">
+                    <div>
+                        <AboutMeBio />
+                    </div>
+                </Col>
+            </Row>
+            <hr />
+            <Row >
                 <Col>
-                    <AboutMeBio />
+                    <AboutMeTable />
+                </Col>
+                <Col className="d-flex align-items-center">
+                    <div>
+                        <CryptoAbout />
+                    </div>
                 </Col>
             </Row>
             <p>This is a test</p>
