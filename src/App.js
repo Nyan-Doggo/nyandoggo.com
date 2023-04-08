0ec58e3ae7ce8151f321ca111fc64e991b5ee519
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-04-08 16:59:35 +0200

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import React from "react";
+import './index.css';
 import { Container } from "react-bootstrap";
 import FrontPage from "./pages/FrontPage";
 
 export default function App() {
   return (
-    <Container fluid className="App">
-      <FrontPage />
-    </Container>
+    <h1 className="text-3xl font-bold text-center text-accent3">
+      Nyandoggo.com 🥒
+    </h1>
+    // <Container fluid className="App">
+    //   <FrontPage />
+    // </Container>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark1: '#2a2b2a',
+        light1: '#e8ddb5',
+        accent1: '#52b2cf',
+        accent2: '#7ee081',
+        accent3: '#d7263d',
+      },
+    },
+  },
+  plugins: [],
+}
+
