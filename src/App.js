30689305ac542b2d60c52eccbf86fc34dd16a52f
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-04-09 15:53:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
     <div className="App h-screen">
       <BrowserRouter>
         <Header />
+        {/*test*/}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/facts" element={<Facts />} />
