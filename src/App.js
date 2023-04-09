0d391cf95195d0112d9abb8665404f0b420745cf
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-04-09 12:46:41 +0200

    seoerated main menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './index.css';
 import Header from "./components/header/Header";
+import MainMenu from "./components/body/MainMenu";
 import i_02Line from './images/02line.png';
 
 export default function App() {
   return (
     <div className="App h-screen">
       <Header />
+
+
       <div className="text-center">
         <p className="font-bold text-cyellow mt-10">
           ATTENTION!
@@ -16,30 +19,9 @@ export default function App() {
         </p>
       </div>
 
-      <div className="w-fit">
+      <MainMenu />
+      
 
-        <div className="flex border-t-2 my-2 border-cblue border-dotted w-fit ml-auto">
-          <div className="p-2">
-            <p>About me 👉👈</p>
-          </div>
-          <div>
-            <p className="p-2 bg-cblue text-cdark shadow-lg hover:bg-clight duration-200">Quick Fax 📠</p>
-            <p className="p-2 bg-cblue text-cdark shadow-lg">Bio 📚</p>
-          </div>
-        </div>
-
-        <div className="flex border-t-2 my-2 border-cyellow border-dotted w-fit">
-          <div className="p-2">
-            <p>Links and stuff 🔗</p>
-          </div>
-          <div>
-            <p className="p-2 bg-cyellow text-cdark shadow-lg hover:bg-clight duration-200">Where to find me 🌐</p>
-            <p className="p-2 bg-cyellow text-cdark shadow-lg">Support me 💸</p>
-          </div>
-        </div>
-
-
-      </div>
 
       <div className="fixed right-0 bottom-0 w-[20vh]">
         <img src={i_02Line}>
---
+++ src/components/body/MainMenu.js
@@ -0,0 +1,32 @@
+function MainMenu() {
+    return (
+        <div className="w-fit">
+
+            <div className="flex border-t-2 my-2 border-cblue border-dotted w-fit ml-auto">
+                <div className="p-2">
+                    <p>About me 👉👈</p>
+                </div>
+                <div>
+                    <p className="p-2 bg-cblue text-cdark shadow-lg hover:bg-clight duration-200">Quick Fax 📠</p>
+                    <p className="p-2 bg-cblue text-cdark shadow-lg">Bio 📚</p>
+                </div>
+            </div>
+
+
+            <div className="flex border-t-2 my-2 border-cyellow border-dotted w-fit">
+                <div className="p-2">
+                    <p>Links and stuff 🔗</p>
+                </div>
+                <div>
+                    <p className="p-2 bg-cyellow text-cdark shadow-lg hover:bg-clight duration-200">Where to find me 🌐</p>
+                    <p className="p-2 bg-cyellow text-cdark shadow-lg">Support me 💸</p>
+                </div>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default MainMenu;
+
