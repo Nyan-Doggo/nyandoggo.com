0fd156bc7fa5d021902e71799b19f85dc1df7d58
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-06-01 16:58:00 +0200

    swapped to next

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
-
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        cdark: '#2a2b2a',
-        clight: '#FBFBF2',
-        cblue: '#52B2CF',
-        cred: '#ff5964',
-        cyellow: '#fdca40',
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [
-    plugin(function ({ addBase }) {
-      addBase({
-        'html': { fontSize: "20px" },
-      })
-    }),
-  ],
+  plugins: [],
 }
-
