f4853bf554e59ba9cc7f4810aebaf1eedf87e516
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-06-30 02:44:22 +0200

    alt added

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       </h3>
 
       <div className="grid grid-cols-3 gap-8 mb-12">
-        <img src="/moomin/1.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
+        <img alt="moomin-cup" src="/moomin/1.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
         <div className="flex hover:bg-gray-600 hover:text-green-100 transition duration-700 px-6">
           <Link className="m-auto" href="/boomer">
             <p className="text-lg">
@@ -21,14 +21,14 @@ export default function Home() {
             </p>
           </Link>
         </div>
-        <img src="/moomin/2.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
+        <img alt="moomin-cup" src="/moomin/2.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
 
         <div className="flex hover:bg-gray-600 hover:text-green-100 transition duration-700 px-6">
           <p className="m-auto text-lg">
             You like code? want to see some cool coding stuff? sure... let me just finnish my nap first.
           </p>
         </div>
-        <img src="/moomin/3.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
+        <img alt="moomin-cup" src="/moomin/3.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
         <div className="flex hover:bg-gray-600 hover:text-green-100 transition duration-700 px-6">
           <p className="m-auto text-lg">
             You know that one very basic song about if you just had one chance... i, i don{"\'"}t think i{"\'"}d take that one chance.
@@ -36,33 +36,33 @@ export default function Home() {
         </div>
 
 
-        <img src="/moomin/4.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
+        <img alt="moomin-cup" src="/moomin/4.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
         <div className="flex hover:bg-gray-600 hover:text-green-100 transition duration-700 px-6">
           <p className="m-auto text-lg">
             I want to reach out, but somehow i think that might just make everything worse.
           </p>
         </div>
-        <img src="/moomin/5.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
+        <img alt="moomin-cup" src="/moomin/5.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
 
         <div className="flex hover:bg-gray-600 hover:text-green-100 transition duration-700 px-6">
           <p className="m-auto text-lg">
             The site isn{"\'"}t done yet. maybe it will be, one day.
           </p>
         </div>
-        <img src="/moomin/6.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
+        <img alt="moomin-cup" src="/moomin/6.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
         <div className="flex hover:bg-gray-600 hover:text-green-100 transition duration-700 px-6">
           <p className="m-auto text-lg">
             i wonder if he still thinks of me
           </p>
         </div>
 
-        <img src="/moomin/7.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
+        <img alt="moomin-cup" src="/moomin/7.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
         <div className="flex hover:bg-gray-600 hover:text-green-100 transition duration-700 px-6">
           <p className="m-auto text-lg">
             Wake me up in an hour or so, i... i just need a quick break
           </p>
         </div>
-        <img src="/moomin/8.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
+        <img alt="moomin-cup" src="/moomin/8.jpg" className="object-cover max-h-[400px] w-full filter grayscale transition hover:grayscale-0 duration-700" />
       </div>
 
     </div>
